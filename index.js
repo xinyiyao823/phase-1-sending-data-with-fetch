08a02615236406a6cd20e630c1da5bfc07476313
+ <+>
2021-09-12 10:36:45 -0500

    commit tree ba783d1c3f90ec41182469ecc8f0ca60136b7ae5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+const BASE_URL = 'http://localhost:3000/users'
+function submitData(userName, userEmail) {
+    fetch(BASE_URL, {
+        method: 'POST',
+        headers: {
+
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "name": userName,
+            "email": userEmail
+        })
+    })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(obj) {
+        return obj.id;
+    })
+}
+submitData()
